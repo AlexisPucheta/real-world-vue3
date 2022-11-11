f1921cdf98e4d6114b9937a007ccc356e78564b6
Alexis Pucheta <apucheta@builderhomesite.com>
2022-11-11 10:10:23 -0300

    setup db.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,6 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
+  rules: {},
 };
